00824e151531204c500061b8d7251e864b9b50b3
RainyCl0ud <galario.rainheart16@gmail.com>
2025-11-24 02:36:45 +0800

    All is fixed

--- resources/views/authorized-ids/index.blade.php
+++ resources/views/authorized-ids/index.blade.php
@@ -9,6 +9,13 @@
                         <p class="text-gray-600 text-xs sm:text-sm mt-1">Manage Student and Faculty ID numbers for registration</p>
                     </div>
                     <div class="flex flex-col sm:flex-row gap-2">
+                          <a href="" 
+                           class="inline-flex items-center px-4 py-2 bg-yellow-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-yellow-700 focus:bg-blue-700 active:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                            <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
+                            </svg>
+                            Import Student File
+                        </a>
                         <a href="{{ route('authorized-ids.create') }}" 
                            class="inline-flex items-center px-4 py-2 bg-blue-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-700 focus:bg-blue-700 active:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition ease-in-out duration-150">
                             <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
